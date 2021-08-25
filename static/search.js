adf1fa0116e04b5a6700439058fb7f560bf520e9
kageru <kageru@encode.moe>
2021-08-25 18:40:09 +0200

    Basic search styling

--- static/search.js
+++ static/search.js
@@ -1,7 +1,6 @@
 const BASE_URL = "https://prd.moe/s/indexes/"
 const PUBLIC_API_KEY = "59e24991b38863d339618208d5217461fa9e7bec568d389f6c6a05bb67c0c0d2";
-function httpGetAsync(callback, input)
-{
+function httpGetAsync(callback, input) {
     let generalSearchUrl = new URL(BASE_URL + 'all/search');
     generalSearchUrl.searchParams.set("q", input);
     let xmlHttp = new XMLHttpRequest();
@@ -15,25 +14,29 @@ function httpGetAsync(callback, input)
 
 const handleSearch = (results) => {
     let content = document.getElementById("content");
-    let newContent = document.getElementById("searchContent");
-    results =  JSON.parse(results);
-    const hits =  results.hits;
+    let newContent = document.getElementById("searchcontent");
+    results = JSON.parse(results);
+    const hits = results.hits;
 
-    if(results.query === "") {
-        if(newContent) document.body.removeChild(newContent);
+    if (results.query === "") {
+        if (newContent) document.body.removeChild(newContent);
         content.removeAttribute("style");
         return;
     }
 
     content.style.display = "none";
-    if(!newContent) {
+    if (!newContent) {
         newContent = document.createElement('div');
-        newContent.setAttribute("id", "searchContent");
+        newContent.setAttribute("id", "searchcontent");
         document.body.appendChild(newContent);
     }
 
     newContent.innerHTML = "";
-    for(let i = 0; i < hits.length; i++){
-        newContent.innerHTML += hits[i].content;
+    for (const hit of hits) {
+        let result = document.createElement("div");
+        result.setAttribute("class", "searchresult");
+	result.innerHTML = hit.content;
+        newContent.appendChild(result);
+        newContent.appendChild(document.createElement("hr"));
     }
 }
