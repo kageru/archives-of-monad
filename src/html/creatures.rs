5032943a832247ca1c66fd13132fcec7fc31dbba
kageru <kageru@encode.moe>
2021-09-14 14:20:31 +0200

    Add more information to bestiary index

--- src/html/creatures.rs
+++ src/html/creatures.rs
@@ -1,7 +1,7 @@
 use super::Template;
 use crate::{
     data::{creature::Creature, traits::TraitDescriptions, HasName},
-    html::{render_trait_legend, render_traits},
+    html::{render_trait_legend, render_traits, render_traits_inline},
 };
 use convert_case::{Case, Casing};
 use itertools::Itertools;
@@ -20,14 +20,15 @@ impl Template<&TraitDescriptions> for Creature {
         let mut page = String::with_capacity(250_000);
         page.push_str("<h1>Creatures</h1><hr><br/><br/>");
         page.push_str("<table class=\"overview\">");
-        page.push_str("<thead><tr><td>Name</td><td>Level</td></tr></thead>");
+        page.push_str("<thead><tr><td>Name</td><td class=\"traitcolumn\">Traits</td><td>Source</td><td>Level</td></tr></thead>");
         for (creature, _) in elements {
             page.push_str(&format!(
-                "<tr><td><a href=\"{}\">{}</a></td><td>{}</td></tr>",
+                "<tr><td><a href=\"{}\">{}</a></td><td class=\"traitcolumn\">",
                 creature.url_name(),
                 creature.name,
-                creature.level,
             ));
+            render_traits_inline(&mut page, &creature.traits);
+            page.push_str(&format!("</td><td>{}</td><td>{}</td></tr>", creature.level, creature.source,));
         }
         page.push_str("</table>");
         page
