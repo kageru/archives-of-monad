325da89fbc741a6e4ada3b075df54e22710a5e13
kageru <kageru@encode.moe>
2022-03-20 19:02:16 +0100

    Use bearer auth for meilisearch

--- static/search.js
+++ static/search.js
@@ -1,5 +1,5 @@
 const BASE_URL = "https://prd.moe/s/indexes/"
-const PUBLIC_API_KEY = "59e24991b38863d339618208d5217461fa9e7bec568d389f6c6a05bb67c0c0d2";
+const PUBLIC_API_KEY = "7Uqp6KjH83c7a89a6c34b1bb654427fef6bb82b7a5b8a9e953322096ad00f7a692008b6c";
 function httpGetAsync(callback, input) {
     let generalSearchUrl = new URL(BASE_URL + 'all/search');
     generalSearchUrl.searchParams.set("q", input);
@@ -8,7 +8,7 @@ function httpGetAsync(callback, input) {
         if (xmlHttp.readyState === 4 && xmlHttp.status === 200) callback(xmlHttp.responseText);
     }
     xmlHttp.open("GET", generalSearchUrl, true);
-    xmlHttp.setRequestHeader('X-Meili-API-Key', PUBLIC_API_KEY);
+    xmlHttp.setRequestHeader('Authorization', `Bearer ${PUBLIC_API_KEY}`);
     xmlHttp.send();
 }
 
